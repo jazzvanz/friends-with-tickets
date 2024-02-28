0c47b04b2a43f4411327122cb990d74179d99357
Jazz Vangeel <jazzvangeel@gmail.com>
2024-02-28 15:39:40 -0500

    replaced basic route with lazy loading for performance

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Feb. 28, 2024
 - Ran create react-app and added typescript
 - Next: Connecting progress to git repo and adding react router.
 - Setting up basic/first draft routes.
+- Added lazy load and example routes
+
+- Add our first draft of routes.
 
 - Will be using a series of sport api's that are likely to be rest but also want to setup my own backend for keeping track.
 
@@ -17,5 +20,8 @@ Feb. 28, 2024
 - TypeScript
 - Webpack
 
+# App Hierarchy
+
 # Before Deploying 
 - React Router: I may need to configure the production server to support client side routing (https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing)
+- Lazy load with router - https://github.com/remix-run/react-router/blob/dev/examples/lazy-loading/src/App.tsx
--- friends-with-tickets/src/App.tsx
+++ friends-with-tickets/src/App.tsx
@@ -1,30 +1,58 @@
+import * as React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+const About = React.lazy(() => import("./pages/About"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 
 export default function App() {
   return (
     <div>
-      <h1>Basic Example</h1>
+      <h1>Lazy Loading Example</h1>
 
       <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
+        This example demonstrates how to lazily load both route elements and
+        even entire portions of your route hierarchy on demand. To get the full
+        effect of this demo, be sure to open your Network tab and watch the new
+        bundles load dynamically as you navigate around.
       </p>
 
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
+      <p>
+        The "About" page is not loaded until you click on the link. When you do,
+        a <code>&lt;React.Suspense fallback&gt;</code> renders while the code is
+        loaded via a dynamic <code>import()</code> statement. Once the code
+        loads, the fallback is replaced by the actual code for that page.
+      </p>
+
+      <p>
+        The "Dashboard" page does the same thing, but takes it even one step
+        further by <em>dynamically defining additional routes</em> once the page
+        loads! Since React Router lets you declare your routes as
+        <code>&lt;Route&gt;</code> elements, you can easily define more routes
+        by placing an additional <code>&lt;Routes&gt;</code> element anywhere
+        further down the element tree. Just be sure the parent route ends with a{" "}
+        <code>*</code> like <code>&lt;Route path="dashboard/*"&gt;</code> in
+        this case.
+      </p>
+
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
+          <Route
+            path="about"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <About />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="dashboard/*"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Dashboard />
+              </React.Suspense>
+            }
+          />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -35,8 +63,6 @@ export default function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
       <nav>
         <ul>
           <li>
@@ -46,19 +72,13 @@ function Layout() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
+            <Link to="/dashboard/messages">Messages (Dashboard)</Link>
           </li>
         </ul>
       </nav>
 
       <hr />
 
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
     </div>
   );
@@ -68,22 +88,6 @@ function Home() {
   return (
     <div>
       <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
     </div>
   );
 }

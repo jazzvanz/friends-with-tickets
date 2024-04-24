ccebe040b6df0a1c1a777810dce16c4ae2fc9303
Jazz Vangeel <jazzvangeel@gmail.com>
2024-04-24 13:24:09 -0400

    basic layout first draft

--- friends-with-tickets/src/pages/ticTacToe.tsx
+++ friends-with-tickets/src/pages/ticTacToe.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 // Requirements
@@ -7,12 +7,127 @@ import styled from 'styled-components'
 // Display the current game status at the top: whose turn it is, winner (if any), or draw.
 // Add a "Reset" button to allow the game to be restarted at any time.
 
-const Headline = styled.h1 `
-    background-color: peachpuff,
+const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 20px 50px;
 `;
 
-export default function ticTacToe() {
+const Headline = styled.h1`
+    background-color: peachpuff;
+`;
+
+const GameStatusContainer = styled.h2`
+    background-color: pink;
+`;
+
+const GameboardLayout = styled.div`
+    background-color: pink;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-gap: 15px;
+    grid-template-columns: repeat(3, 1fr);
+    min-height: 800px;
+    max-width: 800px;
+    margin: 20px auto;
+    width: 100%;
+`;
+
+const GameSqaureStyles = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: blue;
+    color: white;
+    font-size: 60px;
+`;
+
+const GameReset = styled.button`
+    background-color: yellow;
+`;
+
+type Sqaure = {
+    sqaureNumber: number, 
+    selectedBy: string | null
+}
+
+const defaultBoardState: Sqaure[] = [
+    {
+        sqaureNumber: 1,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 2,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 3,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 4,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 5,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 6,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 7,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 8,
+        selectedBy: null,
+    },
+    {
+        sqaureNumber: 9,
+        selectedBy: null,
+    }
+]
+
+export default function TicTacToe() {
+    const [currentPlayer, setCurrentPlayer] = useState('X');
+    const [boardState, setBoardState] = useState([...defaultBoardState]);
+
+    const GameStatus = () => {
+        return (
+            <GameStatusContainer>Player {currentPlayer} Turn</GameStatusContainer>
+        )
+    }
+
+    const GameSqaure = ({selectedBy}: any) => {
+        return (
+            <GameSqaureStyles>
+                <span>{selectedBy}</span>
+            </GameSqaureStyles>
+        )
+    }
+
+
+    const GameResetButton = () => <GameReset>Reset Game</GameReset>;
+
+
+
     return (
-        <Headline>hello</Headline>
+        <Layout>
+            <Headline>Tic Tac Toe - WIP</Headline>
+         
+            <GameStatus />
+
+            <GameboardLayout>
+                 {boardState.map(({selectedBy, sqaureNumber}) => {
+                    return <GameSqaure selectedBy={selectedBy} key={sqaureNumber} />
+                 })}
+            </GameboardLayout>
+
+            <GameResetButton />
+        </Layout>
     )
 }
